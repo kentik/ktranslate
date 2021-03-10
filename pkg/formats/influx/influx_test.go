e977696b5a44bdf5b87f82350502b1b766ebf0b9
Ian Pye <pye@kentik.com>
2021-03-09 18:50:21 -0800

    Fixing tests

--- pkg/formats/influx/influx_test.go
+++ pkg/formats/influx/influx_test.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/kentik/ktranslate/pkg/eggs/logger"
+	lt "github.com/kentik/ktranslate/pkg/eggs/logger/testing"
 	"github.com/kentik/ktranslate/pkg/kt"
 
 	"github.com/stretchr/testify/assert"
@@ -12,8 +14,9 @@ import (
 func TestSeriToInflux(t *testing.T) {
 	serBuf := make([]byte, 0)
 	assert := assert.New(t)
+	l := lt.NewTestContextL(logger.NilContext, t).GetLogger().GetUnderlyingLogger()
 
-	f, err := NewFormat(nil, kt.CompressionNone)
+	f, err := NewFormat(l, kt.CompressionNone)
 	assert.NoError(err)
 
 	res, err := f.To(kt.InputTesting, serBuf)
