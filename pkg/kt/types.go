b2aa5c9a10385caaf4065406e8e854016e5ca986
Ian Pye <pye@kentik.com>
2021-10-29 15:27:38 -0700

    Fixing ifAdminStatus tracking

--- pkg/kt/types.go
+++ pkg/kt/types.go
@@ -34,7 +34,7 @@ const (
 	StringPrefix     = "ks_"
 	PrivateIP        = "Private IP"
 	DropMetric       = "DropMetric"
-	AdminStatus      = StringPrefix + "ifAdminStatus"
+	AdminStatus      = "ifAdminStatus"
 )
 
 type OutputType string
