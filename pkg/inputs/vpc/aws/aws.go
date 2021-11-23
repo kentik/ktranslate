842eeec72be07cf5f842e1eb4fd050dbb842053e
Ian <i3149@users.noreply.github.com>
2021-11-23 14:34:37 -0800

    Adding type to vpc (#205)

--- pkg/inputs/vpc/aws/aws.go
+++ pkg/inputs/vpc/aws/aws.go
@@ -76,6 +76,8 @@ const (
 	AWS_PKT_DST_AWS_SERVICE = "pkt-dst-aws-service"
 	AWS_FLOW_DIRECTION      = "flow-direction"
 	AWS_TRAFFIC_PATH        = "traffic-path"
+
+	AWS_VPC_TYPE = "AWS_VPC"
 )
 
 var (
@@ -468,6 +470,7 @@ func (m *AWSLogLine) ToFlow(log logger.ContextL, topo *AWSTopology) (in *kt.JCHF
 	in.CustomStr["action"] = m.Action
 	in.CustomStr["status"] = m.Status
 	in.CustomStr["kt.from"] = kt.FromLambda
+	in.CustomStr["type"] = AWS_VPC_TYPE
 	in.DeviceName = m.VPCID
 
 	if m.Sample > 0 { // Set sample rate here if we are switching.
