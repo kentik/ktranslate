a0b56f3fa89941cbd2806eabe8d5aff62ffe216f
Ian Pye <pye@kentik.com>
2022-03-07 11:02:45 -0800

    Testing for write as well

--- pkg/inputs/snmp/util/file_test.go
+++ pkg/inputs/snmp/util/file_test.go
@@ -26,6 +26,7 @@ func TestLoadFile(t *testing.T) {
 		":foo":               nil,
 		svr.URL:              content,
 		"s3://foo/bar/a/one": nil,
+		"S3://foo/bar/a/two": nil,
 	}
 
 	// Save test data to local.
@@ -47,6 +48,35 @@ func TestLoadFile(t *testing.T) {
 		} else {
 			assert.NoError(err)
 			assert.Equal(string(expt), string(res), "failed %s", in)
+		}
+	}
+}
+
+func TestWriteFile(t *testing.T) {
+	assert := assert.New(t)
+	content := []byte("aaaa") // Set some test content.
+
+	// Save test data to local.
+	file, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.FailNow()
+	}
+
+	defer os.Remove(file.Name())
+	tests := map[string]bool{
+		":foo":      false,
+		file.Name(): true,
+	}
+
+	ctx := context.Background()
+	for target, good := range tests {
+		err := WriteFile(ctx, target, content, 0644)
+		if !good {
+			assert.Error(err)
+		} else {
+			assert.NoError(err)
+			c, _ := ioutil.ReadFile(target) // This one assumes that we're writting locally.
+			assert.Equal(string(content), string(c), "failed %s", target)
 		}
 	}
 }
