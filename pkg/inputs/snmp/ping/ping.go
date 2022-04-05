b427284657c4aae3ea009b30313fb3b05acec810
Ian <i3149@users.noreply.github.com>
2022-04-05 10:20:31 -0700

    Fix stats per run (#306)

--- pkg/inputs/snmp/ping/ping.go
+++ pkg/inputs/snmp/ping/ping.go
@@ -17,29 +17,49 @@ type Pinger struct {
 	log    logger.ContextL
 	target string
 	pinger *ping.Pinger
+	count  int
+	priv   bool
 }
 
 func NewPinger(log logger.ContextL, target string, inter time.Duration) (*Pinger, error) {
 	p := &Pinger{
 		log:    log,
 		target: target,
-	}
-	pinger, err := ping.NewPinger(target)
-	if err != nil {
-		return nil, err
+		count:  int(inter.Seconds()), // Run 1 ping per sec, for this many seconds.
 	}
 
-	pinger.Interval = inter
 	if os.Getenv(KENTIK_PING_PRIV) == "true" {
 		log.Infof("Running ping service in privileged mode.")
-		pinger.SetPrivileged(true)
+		p.priv = true
+	} else {
+		log.Infof("Running ping service in non privileged mode.")
 	}
-	go pinger.Run()
-	p.pinger = pinger
 
-	return p, nil
+	err := p.Reset()
+	return p, err
 }
 
 func (p *Pinger) Statistics() *ping.Statistics {
 	return p.pinger.Statistics()
+}
+
+func (p *Pinger) Reset() error {
+	pinger, err := ping.NewPinger(p.target)
+	if err != nil {
+		return err
+	}
+
+	// pinger.Interval = inter // Run at 1 per second.
+	pinger.Count = p.count
+	pinger.SetPrivileged(p.priv)
+	p.pinger = pinger
+
+	go func() {
+		err := p.pinger.Run()
+		if err != nil {
+			p.log.Errorf("Cannot ping: %v", err)
+		}
+	}()
+
+	return nil
 }
