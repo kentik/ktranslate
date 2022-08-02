222878297ae3241a4dc38b7da5f05d2579813aff
Tim Danner <tdanner@users.noreply.github.com>
2022-08-02 17:01:45 -0500

    Put \n at the end of every influx line, not just between them (#372)

--- pkg/formats/influx/influx_test.go
+++ pkg/formats/influx/influx_test.go
@@ -23,7 +23,9 @@ func TestSeriToInflux(t *testing.T) {
 	assert.NoError(err)
 	assert.NotNil(res)
 
-	pts := strings.Split(string(res.Body), "\n")
+	assert.Equal(byte('\n'), res.Body[len(res.Body)-1])
+
+	pts := strings.Split(string(res.Body[:len(res.Body)-1]), "\n")
 	assert.Equal(len(pts), len(kt.InputTesting))
 }
 
