8c18e991392c2b51d7ffb79194f94e557591b19a
Ian Pye <pye@kentik.com>
2022-08-09 10:58:32 -0700

    Fixing printf style logging for netflow callback logging.

--- pkg/inputs/flow/log.go
+++ pkg/inputs/flow/log.go
@@ -11,30 +11,30 @@ type KentikLog struct {
 }
 
 func (l *KentikLog) Printf(f string, vars ...interface{}) {
-	l.l.Infof(f, vars)
+	l.l.Infof(f, vars...)
 }
 func (l *KentikLog) Errorf(f string, vars ...interface{}) {
-	l.l.Errorf(f, vars)
+	l.l.Errorf(f, vars...)
 }
 func (l *KentikLog) Warnf(f string, vars ...interface{}) {
-	l.l.Warnf(f, vars)
+	l.l.Warnf(f, vars...)
 }
 func (l *KentikLog) Warn(vars ...interface{}) {
-	l.l.Warnf("%v", vars)
+	l.l.Warnf("%v", vars...)
 }
 func (l *KentikLog) Error(vars ...interface{}) {
-	l.l.Errorf("%v", vars)
+	l.l.Errorf("%v", vars...)
 }
 func (l *KentikLog) Debug(vars ...interface{}) {
-	l.l.Debugf("%v", vars)
+	l.l.Debugf("%v", vars...)
 }
 func (l *KentikLog) Debugf(f string, vars ...interface{}) {
-	l.l.Debugf(f, vars)
+	l.l.Debugf(f, vars...)
 }
 func (l *KentikLog) Infof(f string, vars ...interface{}) {
-	l.l.Infof(f, vars)
+	l.l.Infof(f, vars...)
 }
 func (l *KentikLog) Fatalf(f string, vars ...interface{}) {
-	l.l.Errorf(f, vars)
+	l.l.Errorf(f, vars...)
 	panic(fmt.Sprintf(f, vars...))
 }
