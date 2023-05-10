90a0a515f63502bb41063572221eb99c873b5063
Ian Pye <ianpye@gmail.com>
2023-05-10 12:17:53 -0700

    Fixing case where a disco with mapped in secrets will leak secrets

--- pkg/kt/snmp_test.go
+++ pkg/kt/snmp_test.go
@@ -1,10 +1,12 @@
 package kt
 
 import (
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
 )
 
 func TestIsPollReady(t *testing.T) {
@@ -33,4 +35,45 @@ func TestGetName(t *testing.T) {
 		Name: "bar",
 	}
 	assert.Equal(t, "bar", mib.GetName())
+}
+
+func TestSNMPV3(t *testing.T) {
+	input := []byte(`
+user_name: mabel
+authentication_protocol: MD5
+authentication_passphrase: password123
+privacy_protocol: AES
+privacy_passphrase: password123
+context_engine_id: aaa
+context_name: ""
+`)
+
+	ms := V3SNMPConfig{}
+	err := yaml.Unmarshal(input, &ms)
+	assert.NoError(t, err)
+	assert.Equal(t, "password123", ms.AuthenticationPassphrase)
+
+	ser, err := yaml.Marshal(&ms)
+	assert.NoError(t, err)
+	assert.Equal(t, strings.TrimSpace(string(input)), strings.TrimSpace(string(ser)))
+
+	input = []byte(`
+user_name: mabel
+authentication_protocol: MD5
+authentication_passphrase: ${foo}
+privacy_protocol: AES
+privacy_passphrase: password123
+context_engine_id: ${bar}
+context_name: ""
+`)
+	t.Setenv("foo", "password123")
+	t.Setenv("bar", "1234")
+	err = yaml.Unmarshal(input, &ms)
+	assert.NoError(t, err)
+	assert.Equal(t, "password123", ms.AuthenticationPassphrase)
+	assert.Equal(t, "${foo}", ms.origConf["AuthenticationPassphrase"])
+
+	ser, err = yaml.Marshal(&ms)
+	assert.NoError(t, err)
+	assert.Equal(t, strings.TrimSpace(string(input)), strings.TrimSpace(string(ser)))
 }
