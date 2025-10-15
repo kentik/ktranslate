4d1bef3a427baf309c8055d75efb1e3b4dac97a8
Ian <i3149@users.noreply.github.com>
2025-10-15 16:21:02 -0700

    Allowing user to override the default oid for IfHighSpeed if needed. (#852)
    * Allowing user to override the default oid for IfHighSpeed if needed.
    
    * forcing ifSpeed to be same units as ifHighSpeed

--- pkg/inputs/snmp/metadata/interface_metadata.go
+++ pkg/inputs/snmp/metadata/interface_metadata.go
@@ -31,6 +31,7 @@ const (
 
 	SNMP_HUAWEI_ID_MAP_OID     = "1.3.6.1.4.1.2011.5.25.110.1.2.1.2"
 	MIN_WORKING_HUAWEI_VERSION = 5
+	KT_IFSPEED_OID             = "KT_IFSPEED_OID"
 )
 
 var (
@@ -39,7 +40,7 @@ var (
 		// change the order unless you know what you're doing.  Add new ones on
 		// the end.
 		m := orderedmap.NewOrderedMap()
-		m.Set("1.3.6.1.2.1.31.1.1.1.15", SNMP_ifSpeed)       // Index ifIndex
+		m.Set(getIfSpeedOid(), SNMP_ifSpeed)                 // Index ifIndex. Uses IfHighSpeed by default but overridable if needed.
 		m.Set("1.3.6.1.2.1.4.20.1.2", SNMP_ipAdEntIfIndex)   // Index ipAddr
 		m.Set("1.3.6.1.2.1.4.20.1.3", SNMP_ipAdEntNetMask)   // Index ipAddr
 		m.Set("1.3.6.1.2.1.55.1.8.1.2", SNMP_ipv6AddrPrefix) // Index ipv6IfIndex, ipv6Addr
@@ -47,6 +48,7 @@ var (
 		return m
 	}()
 
+	isIfSpeed          = false
 	getHuaweiVersionRE = regexp.MustCompile(`Version (\d)\.(\d+)`)
 )
 
@@ -55,6 +57,14 @@ type InterfaceMetadata struct {
 	mibs          map[string]*kt.Mib
 	interfaceOids *orderedmap.OrderedMap
 	debug         bool
+}
+
+func getIfSpeedOid() string {
+	speed := kt.LookupEnvString(KT_IFSPEED_OID, "1.3.6.1.2.1.31.1.1.1.15")
+	if speed != "1.3.6.1.2.1.31.1.1.1.15" {
+		isIfSpeed = true
+	}
+	return speed
 }
 
 func NewInterfaceMetadata(interfaceMetadataMibs map[string]*kt.Mib, log logger.ContextL, debug bool) *InterfaceMetadata {
@@ -193,6 +203,9 @@ func (im *InterfaceMetadata) Poll(ctx context.Context, conf *kt.SnmpDeviceConfig
 				case SNMP_ifSpeed:
 					value := gosnmp.ToBigInt(variable.Value).Uint64()
 					data.Speed = value
+					if isIfSpeed {
+						data.Speed = uint64(float64(data.Speed) / float64(1000000))
+					}
 				case SNMP_ipv6AddrPrefix:
 					switch variable.Type {
 					case gosnmp.Integer:
